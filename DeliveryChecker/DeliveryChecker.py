69e8cee96bb0feac48a69ff7cc07a5c7675cdec2
hjl <lhanj296@gmail.com>
2022-11-26 10:08:34 +0900

    Add files via upload

--- DeliveryChecker/DeliveryChecker.py
+++ DeliveryChecker/DeliveryChecker.py
@@ -1,1 +1,36 @@
+import ui
+import re
+import clipboard
+import urllib.request
+import webbrowser
+from urllib.parse import quote
+import sys
 
+
+def listToString(str_list):
+	result =""
+	for s in str_list:
+		result += s + " "
+		return result.strip()
+
+def tableview_shipping_did_select(sender):
+  value = sender.selected_row
+  global name 
+  name = sender.items[value]
+  defaultUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query='  + quote(str(name)) +listToString(newList)
+  webbrowser.open(defaultUrl)
+  v.close()
+
+v = ui.load_view()
+v.name = 'Delivery Checker'
+#clipboard.get_image()
+clipboardData = clipboard.get()
+numbersList = re.findall(r'\d+', clipboardData)
+
+newList = [x for x in numbersList if len(x)>10]
+
+if listToString(newList) is None:
+  v.close()
+  sys.exit()
+else:
+  v.present('sheet')
